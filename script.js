e120316e4cebdecf7dd4eedf12decab328a4a4a4
Adrienne Knight <adriennek1@gmail.com>
2020-06-22 08:26:21 -0600

    worked on functions in the js

---
+++ script.js
@@ -0,0 +1,42 @@
+// Assignment Code
+const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+  'y', 'z'
+];
+const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_',
+  '.'];
+
+var generateBtn = document.getElementById("generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.getElementById("password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  let charReturn = prompt("Pick a length for your password between 8 and 128 characters")
+  let charReturnNum = Number(charReturn)
+
+  if (charReturnNum <= 128 && charReturnNum >= 8) {
+    let uppercase = confirm("Do you want uppercase characters?");
+    let lowercase = confirm("Do you want lowercase characters?");
+    let specialChar = confirm("Do you want special characters?");
+    let numbers = confirm("Do you want numbers?");
+  } else { alert("Please select length between 8 and 128") }
+
+}
+
+function getRandomArrayIndex(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length)
+  return arr[randomIndex]
+}
+
+function charMix(){}
